66d19c1acb0db366fa40a2a884a77493b2dd2268
Jeff Taggart <jeff@jetaggart.com>
2014-08-23 14:11:12 -0600

    Complete log parsing problem

--- src/Homework2/LogAnalysis.hs
+++ src/Homework2/LogAnalysis.hs
@@ -8,8 +8,9 @@ parseMessage :: String -> LogMessage
 parseMessage string = case words string of
   ("I":infoNumber:message) -> LogMessage Info (read infoNumber) $ unwords message
   ("E":severity:infoNumber:message) -> LogMessage (Error $ read severity) (read infoNumber) $ unwords message
+  ("W":infoNumber:message) -> LogMessage Warning (read infoNumber) $ unwords message
   otherwise -> Unknown string
 
-parse = undefined
-
+parse :: String -> [LogMessage]
+parse = map parseMessage . lines
 
--- test/Homework2/LogAnalysisSpec.hs
+++ test/Homework2/LogAnalysisSpec.hs
@@ -21,7 +21,17 @@ spec = do
   describe "parse" $ do
     it "parses the first line of error.log correctly" $ do
       result <- testParse parse 1 "data/error.log"
-      result `shouldBe` [LogMessage Info 5053 "con ing!"]
+      head result `shouldBe` LogMessage Info 5053 "pci_id: con ing!"
+
+    it "parses the second line of error.log correctly" $ do
+      (_:result:_) <- testParse parse 2 "data/error.log"
+      
+      result `shouldBe` LogMessage Info 4681 "ehci 0xf43d000:15: regista14: [0xbffff 0xfed nosabled 00-02] Zonseres: brips byted nored)"
+  
+    it "parses a warning line of error.log correctly" $ do
+      (_:_:result:_) <- testParse parse 3 "data/error.log"
+      
+      result `shouldBe` LogMessage Warning 3654 "e8] PGTT ASF! 00f00000003.2: 0x000 - 0000: 00009dbfffec00000: Pround/f1743colled"
 
 main :: IO ()
 main = hspec spec
