800d183e2bb100cbc468bdd8e801ce7a23a16f9a
Jeff Taggart <jeff@jetaggart.com>
2014-08-23 14:18:03 -0600

    Line wrapping

--- src/Homework2/LogAnalysis.hs
+++ src/Homework2/LogAnalysis.hs
@@ -6,9 +6,12 @@ import Homework2.Log
 
 parseMessage :: String -> LogMessage
 parseMessage string = case words string of
-  ("I":infoNumber:message) -> LogMessage Info (read infoNumber) $ unwords message
-  ("E":severity:infoNumber:message) -> LogMessage (Error $ read severity) (read infoNumber) $ unwords message
-  ("W":infoNumber:message) -> LogMessage Warning (read infoNumber) $ unwords message
+  ("I":infoNumber:message) ->
+    LogMessage Info (read infoNumber) $ unwords message
+  ("E":severity:infoNumber:message) ->
+    LogMessage (Error $ read severity) (read infoNumber) $ unwords message
+  ("W":infoNumber:message) ->
+    LogMessage Warning (read infoNumber) $ unwords message
   otherwise -> Unknown string
 
 parse :: String -> [LogMessage]
